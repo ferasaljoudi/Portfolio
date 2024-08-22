0a440deb954fb0d869f14e396f695fefc152108e
Feras Aljoudi <feras.aljoudi@gmail.com>
2024-08-21 20:58:20 -0600

    Update

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -4,7 +4,7 @@ import './Resume.css';
 const Resume = () => {
   return (
     <section id="resume" className="resume-section">
-      <h2>Resume</h2>
+      <h1>Resume</h1>
       <p>Skills: C, Python, JavaScript.</p>
       <p>Experience: ...</p>
       <p>Projects: ....</p>
